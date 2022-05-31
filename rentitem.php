162d273085ee77ceaf824ae3b4ef2442ae25e842
shiki <33571437+Shikiiii@users.noreply.github.com>
2022-05-31 12:26:40 +0300

    Create rentitem.php

---
+++ rentitem.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rented</title>
+</head>
+<body>
+    <h1>Item has been marked as rented.</h1>
+    <a href="http://127.0.0.1:8000/">
+        <input type="button" value="Go back">
+    </a>
+</body>
+</html>
