578fdd0d431b3880cac497369dc9473e6a0f5cbd
shiki <33571437+Shikiiii@users.noreply.github.com>
2022-05-31 12:24:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,39 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return HTMLResponse(str(open("test.html").read()), status_code=200)
+
+@app.get("/addnewitem")
+async def root():
+    return HTMLResponse(str(open("addnewitem.html").read()), status_code=200)
+
+@app.get("/additem.php")
+async def root():
+    return HTMLResponse(str(open("additem.php").read()), status_code=200)
+
+@app.get("/removeitem")
+async def root():
+    return HTMLResponse(str(open("removeitem.html").read()), status_code=200)
+
+@app.get("/remitem.php")
+async def root():
+    return HTMLResponse(str(open("remitem.php").read()), status_code=200)
+
+@app.get("/rentitem")
+async def root():
+    return HTMLResponse(str(open("rentitem.html").read()), status_code=200)
+
+@app.get("/rentitem.php")
+async def root():
+    return HTMLResponse(str(open("rentitem.php").read()), status_code=200)
+
+@app.get("/unrentitem")
+async def root():
+    return HTMLResponse(str(open("unrentitem.html").read()), status_code=200)
+
+@app.get("/unrentitem.php")
+async def root():
+    return HTMLResponse(str(open("unrentitem.php").read()), status_code=200)
+
+@app.get("/search.php")
+async def root():
+    return HTMLResponse(str(open("search.php").read()), status_code=200)
