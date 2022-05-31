33c2ea84af4cbcdf949873e2f3ff0b5524a627cf
shiki <33571437+Shikiiii@users.noreply.github.com>
2022-05-31 12:25:38 +0300

    Create additem.php

---
+++ additem.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Added</title>
+</head>
+<body>
+    <h1>Your item has been added.</h1>
+    <a href="http://127.0.0.1:8000/">
+        <input type="button" value="Go back">
+    </a>
+</body>
+</html>
