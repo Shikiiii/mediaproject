29aeabfc40b7bf7fff36804200a9c1a181055a2a
shiki <33571437+Shikiiii@users.noreply.github.com>
2022-05-31 12:27:43 +0300

    Create unrentitem.php

---
+++ unrentitem.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Removed</title>
+</head>
+<body>
+    <h1>Item has been unmarked as rented.</h1>
+    <a href="http://127.0.0.1:8000/">
+        <input type="button" value="Go back">
+    </a>
+</body>
+</html>
